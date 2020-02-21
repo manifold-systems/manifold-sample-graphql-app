7c7af0c8244df9cc168953da4a7de9a4bc27ad5c
scott <rsmckinney@hotmail.com>
2020-02-20 21:12:55 -0800

    use latest manifold

--- src/test/java/manifold/test/MovieServerTest.java
+++ src/test/java/manifold/test/MovieServerTest.java
@@ -1,27 +1,16 @@
 package manifold.test;
 
-import manifold.api.json.IJsonBindingsBacked;
-import manifold.api.json.Json;
-import manifold.graphql.request.Executor;
 import manifold.graphql.sample.schema.movies;
 import manifold.graphql.sample.schema.queries;
 import manifold.graphql.sample.server.MovieServer;
-import manifold.util.ReflectUtil;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import spark.Spark;
-import spark.servlet.SparkApplication;
-
-import javax.script.Bindings;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
 
 import static manifold.graphql.sample.schema.movies.Genre.*;
 import static manifold.graphql.sample.schema.queries.*;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 /**
  * Test queries against MovieServer.
@@ -72,36 +61,20 @@ public class MovieServerTest {
     assertEquals("Steve McQueen", actor.getName());
   }
 
-  //
-  // Java 8 without `var`
-  //
-//  @Test
-//  public void testMovieQueryNonEmpty() {
-//    MovieQuery movieQuery = MovieQuery.builder().withGenre(Romance).build();
-//    MovieQuery.Result result = movieQuery.request(ENDPOINT).post();
-//    List<MovieQuery.Result.movies> movies = result.getMovies();
-//    assertEquals(1, movies.size());
-//    MovieQuery.Result.movies movie = movies.get(0);
-//    assertTrue(movie.getGenre().contains(Romance));
-//    assertEquals("The Getaway", movie.getTitle());
-//    assertEquals("Steve McQueen", movie.getCast().get(0).getActor().getName());
-//  }
-//
-//  @Test
-//  public void testMovieQueryEmpty() {
-//    MovieQuery movieQuery = MovieQuery.builder().withGenre(Horror).build();
-//    MovieQuery.Result result = movieQuery.request(ENDPOINT).post();
-//    List<MovieQuery.Result.movies> movies = result.getMovies();
-//    assertTrue(movies.isEmpty());
-//  }
-//
-//  @Test
-//  public void testActorQuery() {
-//    ActorQuery actorQuery = ActorQuery.builder("Le Mans").withGenre(Action).build();
-//    ActorQuery.Result result = actorQuery.request(ENDPOINT).post();
-//    List<ActorQuery.Result.actors> actors = result.getActors();
-//    assertEquals(1, actors.size());
-//    ActorQuery.Result.actors actor = actors.get(0);
-//    assertEquals("Steve McQueen", actor.getName());
-//  }
+  @Test
+  public void testGraphQLFragments() {
+    // get "The Great Escape" movie
+    MovieQuery movieQuery = MovieQuery.builder().withTitle("The Great Escape").build();
+    MovieQuery.Result result = movieQuery.request(ENDPOINT).post();
+    var movies = result.getMovies();
+    MovieQuery.Result.movies movie = movies.get(0);
+    assertEquals("The Great Escape", movie.getTitle());
+    // compare role ids from movie cast list
+    assertEquals( 2, movie.getCast().size());
+    var mq = CompareRoles.builder(movie.getCast().get(0).getId(), movie.getCast().get(1).getId()).build();
+    var comparison = mq.request(ENDPOINT).post();
+    var left = comparison.getLeftComparison();
+    var right = comparison.getRightComparison();
+    assertNotEquals(left.getActor().getName(), right.getActor().getName());
+  }
 }
